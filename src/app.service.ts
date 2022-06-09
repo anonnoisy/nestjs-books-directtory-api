40612d0bbb6136c5920409838049c9c3710cc943
Rifky Sultan Karisma Andriansyah <batuhc105@gmail.com>
2022-06-10 04:20:19 +0700

    update index endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): object {
+    return {
+      "name": "Books Directory API",
+      "description": "Books Directory API documentation",
+      "version": "1.0"
+    };
   }
 }
