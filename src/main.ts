3147b4696606e379983011704030cbe81e13b557
Rifky Sultan Karisma Andriansyah <batuhc105@gmail.com>
2022-06-10 03:33:36 +0700

    add swagger open api

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -9,6 +10,16 @@ async function bootstrap() {
       logger: true,
     })
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Books Directory')
+    .setDescription('The Books Directory API description')
+    .setVersion('1.0')
+    .addTag('books')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
